5da64cb1d31f300aaf9fe5d0e4fffb427fff4dda
sracedorfman <83509848+sracedorfman@users.noreply.github.com>
2022-11-02 12:29:29 -0400

    fixed stalk measuring and drawing

--- stalk.js
+++ stalk.js
@@ -5,16 +5,16 @@ class Stalk {
     this.up = random(1) > 0.5;
     this.strokeColor = COLOR;
     this.weight = WEIGHT;
-    this.numTris = random(5);
-    this.height = this.up ? min(this.posY, random(50, 100)) : min(height-this.posY, random(50, 100));
-    this.dim = random(5, this.height/this.numTris*0.8);
+    this.numTris = floor(random(1,6));
+    this.height = this.up ? min(this.posY-random(5,10), random(50, 100)) : min(height-this.posY-random(5,10), random(50, 100));
+    this.dim = random(5, this.height/this.numTris*0.6);
     this.triangle = new Triangle(100,100,30,false,false);
     this.triangles = [];
     for (let i = 0; i < this.numTris; i++) {
       this.triangles.push(new Triangle(0, 0, this.dim, true, this.up));
     }
     this.nextPosX = this.posX+random(100)+this.dim+10;
-    this.nextPosY = this.posY + (this.up ? -1 : 1)*(this.height);
+    this.nextPosY = this.posY + (this.up ? -1 : 1)*(this.height-this.dim);
   }
 
   render() {
@@ -23,15 +23,16 @@ class Stalk {
     strokeWeight(this.weight);
     push();
       translate(this.posX, this.posY);
-      line(0, 0, 0, this.up ? -this.height : this.height);
       push();
-        translate(0, this.up ? -this.height : this.height);
-        for (let i = 0; i < this.numTris; i++) {
-          this.triangles[i].render();
-          console.log(this.triangles[i]);
-          translate(0, this.up ? this.dim : -this.dim);
-        }
+      translate(0, this.up ? -(this.height-this.dim) : this.height-this.dim);
+      console.log(this);
+      for (let i = 0; i < this.numTris; i++) {
+        this.triangles[i].render();
+        console.log(this.triangles[i]);
+        translate(0, this.up ? this.dim : -this.dim);
+      }
       pop();
+      line(0, 0, 0, this.up ? -this.height : this.height);
       line(0, this.nextPosY-this.posY, this.nextPosX-this.posX, this.nextPosY-this.posY);
     pop();
   }
